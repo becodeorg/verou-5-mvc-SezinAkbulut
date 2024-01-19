7fa4b3b2276da91ee094b7dfec1e8583f96e0bcb
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-19 12:20:06 +0100

    add new property -author- to Model

--- Model/article.php
+++ Model/article.php
@@ -10,14 +10,16 @@ class Article
     public string $title;
     public ?string $description;
     public ?string $publishDate;
+    public string $author;
 
-    public function __construct(?int $id, ?string $photo, string $title, ?string $description, ?string $publishDate)
+    public function __construct(?int $id, ?string $photo, string $title, ?string $description, ?string $publishDate, string $author)
     {
         $this->id = $id;
         $this->photo = $photo;
         $this->title = $title;
         $this->description = $description;
         $this->publishDate = $publishDate;
+        $this->author = $author;
     }
 
     public function formatPublishDate($format = 'd-m-Y')
