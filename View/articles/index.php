6d681769b851e2716f8d050f2f7974bbfda6d12b
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-19 12:24:17 +0100

    update View/articles/index.ph with new propertyp

--- View/articles/index.php
+++ View/articles/index.php
@@ -5,7 +5,7 @@
     <h1>Articles</h1>
     <ul>
         <?php foreach ($this->articles as $article) : ?>
-            <li><?= $article->title ?> (<?= $article->formatPublishDate() ?>)</li>
+            <li><?= $article->title ?> - By <?= $article->author ?> (<?= $article->formatPublishDate() ?>)</li>
             <a href="index.php?action=articles-show&id=<?= $article->id ?>">More info</a>
         <?php endforeach; ?>
     </ul>
